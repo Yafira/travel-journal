374a9d88a32da1eae34067038c37973406a09b98
Yafira <yafira.martinez@lc.cuny.edu>
2022-02-25 22:21:40 -0500

    changed direction of posts

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -5,9 +5,11 @@ export default function Card(props) {
 		<div className='post'>
 			<img src={`../assets/${props.coverImg}`} className='post--image' />
 			<div className='post--details'>
-				<img src='../assets/pin.png'></img>
-				<span>{props.location}</span>
-				<img src={`../assets/${props.flag}`} className='post--flag' />
+				<div className='country--details'>
+					<img src='../assets/pin.png'></img>
+					<span>{props.location}</span>
+					<img src={`../assets/${props.flag}`} className='post--flag' />
+				</div>
 				<p className='post--title'>{props.title}</p>
 				<p className='post--date'>{props.date}</p>
 				<span className='post--desc'>{props.description}</span>
