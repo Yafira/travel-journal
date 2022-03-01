1b49c54fbf3482fe1d573c92afaa6c3be503327f
Yafira <yafira.martinez@lc.cuny.edu>
2022-02-28 21:17:29 -0500

    added alt props to images

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -4,12 +4,20 @@ import './post.css';
 export default function Card(props) {
 	return (
 		<div className='post'>
-			<img src={`../assets/${props.coverImg}`} className='post--image' />
+			<img
+				src={`../assets/${props.coverImg}`}
+				className='post--image'
+				alt='location-img'
+			/>
 			<div className='post--details'>
 				<div className='country--details'>
-					<img src='../assets/pin.png' />
+					<img src='../assets/pin.png' alt='location-pin' />
 					<span>{props.location}</span>
-					<img src={`../assets/${props.flag}`} className='post--flag' />
+					<img
+						src={`../assets/${props.flag}`}
+						className='post--flag'
+						alt='flag'
+					/>
 					<a href={props.direction} className='post--map'>
 						View on Google Maps
 					</a>
