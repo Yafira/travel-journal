91124c4b903647d39522d3ad9da5450b03cb34a1
Yafira <yafira.martinez@lc.cuny.edu>
2022-02-28 21:15:17 -0500

    made revisions in file structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './style.css';
-import './components/Post/post.css';
-import './components/Navbar/navbar.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
